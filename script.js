1fb2994dbfae3823bf056d8bb36df908d4cffb94
temsahak-rgb <javanvatemsah@gmail.com>
2026-07-29 12:36:15 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const app = document.getElementById("app");
 
+app.innerHTML = `
+
+<h1>Français avec Dino</h1>
+
+<p>
+
+Bienvenue
+
+</p>
+
+<button id="start">
+
+Commencer
+
+</button>
+
+`;
+
+document
+.getElementById("start")
+.addEventListener("click",()=>{
+
+alert("Ça fonctionne !");
+
+});
