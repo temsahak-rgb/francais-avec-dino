45f8e800c1a39de29aed2abf97bc62b25740beba
temsahak-rgb <javanvatemsah@gmail.com>
2026-07-30 16:01:59 +0330

    Update placementEngine.js

--- placementEngine.js
+++ placementEngine.js
@@ -1,1 +1,10 @@
+let placementQuestions = [];
 
+async function loadPlacementQuestions() {
+    const response = await fetch("./data/placement.json");
+    placementQuestions = await response.json();
+}
+
+function getPlacementQuestions() {
+    return placementQuestions;
+}
