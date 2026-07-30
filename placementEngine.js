a933f5f1e603868cf6a26f3ce0694d82f3aafb5f
temsahak-rgb <javanvatemsah@gmail.com>
2026-07-30 16:07:01 +0330

    Update placementEngine.js

--- placementEngine.js
+++ placementEngine.js
@@ -1,8 +1,12 @@
 let placementQuestions = [];
 
 async function loadPlacementQuestions() {
-    const response = await fetch("./data/placement.json");
-    placementQuestions = await response.json();
+    try {
+        const response = await fetch("./data/placement.json");
+        placementQuestions = await response.json();
+    } catch (error) {
+        console.error("خطا در بارگذاری سوالات:", error);
+    }
 }
 
 function getPlacementQuestions() {
